81e8b2c667d52a2c5ddbbcd7054b53a4a10e04dd
João Francisco Cocca Fukuda <joao.fukuda@usp.br>
2019-12-02 02:31:43 -0300

    Jogadores não "param" quando encostam em paredes

--- src/OUTPUT.cpp
+++ src/OUTPUT.cpp
@@ -136,8 +136,11 @@ void *update(void *null)
 
 			if(map[p.pos_y][p.pos_x] == '#')
 			{
-				p.pos_x = late_pos_x;
-				p.pos_y = late_pos_y;
+				double diff_x = p.pos_x - (double)(int)p.pos_x, diff_y = p.pos_x - (double)(int)p.pos_y;
+				if(diff_x < diff_y)
+					p.pos_y = late_pos_y;
+				else
+					p.pos_x = late_pos_x;
 			} else if(map[p.pos_y][p.pos_x] == 'A')
 			{
 				show_message("Numero 1");
